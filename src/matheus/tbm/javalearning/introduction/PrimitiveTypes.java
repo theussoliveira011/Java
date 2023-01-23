e2eadd93693024d1ce022562cf6facce9d05ac7c
Matheus Oliveira <matheusfotografia987@gmail.com>
2023-01-23 14:37:45 -0300

    PrimitiveTypes

--- src/matheus/tbm/javalearning/introduction/PrimitiveTypes.java
+++ src/matheus/tbm/javalearning/introduction/PrimitiveTypes.java
@@ -1,2 +1,9 @@
-package matheus.tbm.javalearning.introduction;public class PrimitiveTypes {
+package matheus.tbm.javalearning.introduction;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        // int, double, float, long
+        int age = 10;
+        System.out.println("my age is: " + age+" years");
+    }
 }
