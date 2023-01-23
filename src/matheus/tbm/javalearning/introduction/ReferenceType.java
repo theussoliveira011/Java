3f52efbd687a02d3b079e0798cb6387ff090ec31
Matheus Oliveira <matheusfotografia987@gmail.com>
2023-01-23 15:27:15 -0300

    /#string-reference-type

---
+++ src/matheus/tbm/javalearning/introduction/ReferenceType.java
@@ -0,0 +1,12 @@
+package matheus.tbm.javalearning.introduction;
+
+public class ReferenceType {
+    public static void main(String[] args) {
+        // String -> Is a class in Java.
+
+        String name = "Matheus";
+        String text = "A large text";
+
+        System.out.println(name);
+    }
+}
