24bb1502139d8881b69d4e34724a07cbf9116a91
Matheus Oliveira <matheusfotografia987@gmail.com>
2023-01-24 16:38:37 -0300

    /#methods

---
+++ src/matheus/tbm/javalearning/javacore/Bintroductionmethods/dominio/Calculadora.java
@@ -0,0 +1,12 @@
+package matheus.tbm.javalearning.javacore.Bintroductionmethods.dominio;
+
+public class Calculadora {
+
+    public void somaDoisNumeros(){
+        System.out.println(10+10);
+    }
+
+    public void subtraiDoisNumeros(){
+        System.out.println(21-2);
+    }
+}
---
+++ src/matheus/tbm/javalearning/javacore/Bintroductionmethods/test/CalculadoraTest.java
@@ -0,0 +1,11 @@
+package matheus.tbm.javalearning.javacore.Bintroductionmethods.test;
+
+import matheus.tbm.javalearning.javacore.Bintroductionmethods.dominio.Calculadora;
+
+public class CalculadoraTest {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+        calculadora.subtraiDoisNumeros();
+    }
+}
