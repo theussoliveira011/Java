c7b65b86e3f4f20b3c0c87f9377a1d859b357550
Matheus Oliveira <matheusfotografia987@gmail.com>
2023-01-24 08:52:37 -0300

    operators

---
+++ src/matheus/tbm/javalearning/introduction/Operators.java
@@ -0,0 +1,13 @@
+package matheus.tbm.javalearning.introduction;
+
+public class Operators {
+
+    public static void main(String[] args) {
+        /*
+        * Arithmetic -> + - / % *
+        * Relational -> > >= <= < == != in Java don't exist (===)
+        * Logical -> &&(AND) ||(OR) !(NOT)
+        * Assignment -> = += -= *= /= %=
+        * */
+    }
+}
