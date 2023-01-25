496d50d9672d3c7ded97575db99f8c0fa3b45cfa
Matheus Oliveira <matheusfotografia987@gmail.com>
2023-01-25 15:53:23 -0300

    /#methods-get-set

---
+++ src/matheus/tbm/javalearning/javacore/Bintroductionmethods/test/PessoaTest.java
@@ -0,0 +1,16 @@
+package matheus.tbm.javalearning.javacore.Bintroductionmethods.test;
+
+import matheus.tbm.javalearning.javacore.Bintroductionmethods.dominio.Pessoa;
+
+public class PessoaTest {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setName("Samba");
+        pessoa.setAge(28);
+        pessoa.setPassword("querosene");
+
+        System.out.println(pessoa.getName());
+        System.out.println(pessoa.getAge());
+        System.out.println(pessoa.getPassword());
+    }
+}
