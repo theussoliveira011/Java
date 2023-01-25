f84c3d431d8e326a72736de3962f3d46428cb24f
Matheus Oliveira <matheusfotografia987@gmail.com>
2023-01-25 12:15:06 -0300

    /#simple-calculador

--- src/matheus/tbm/javalearning/javacore/Bintroductionmethods/dominio/Calculadora.java
+++ src/matheus/tbm/javalearning/javacore/Bintroductionmethods/dominio/Calculadora.java
@@ -2,11 +2,41 @@ package matheus.tbm.javalearning.javacore.Bintroductionmethods.dominio;
 
 public class Calculadora {
 
-    public void somaDoisNumeros(){
-        System.out.println(10+10);
+    public void somaDoisNumeros(double a, double b){
+        System.out.println(a + b);
     }
 
-    public void subtraiDoisNumeros(){
-        System.out.println(21-2);
+    public double subtraiDoisNumeros(double a, double b){
+        double operation = a - b;
+        double result = this.verificaNumeroPar(operation);
+
+        return result;
+    }
+
+    public void divideDoisNumeros(double a, double b){
+        System.out.println(a / b);
+    }
+
+    public void multiplicaDoisNumeros(double a, double b){
+        System.out.println(a * b);
+    }
+
+    public double restoDoisNumeros(double a, double b){
+        double operation = a % b;
+        double result = this.verificaNumeroPar(operation);
+
+        if(b == 0){
+            return 0;
+        }
+        return result;
+    }
+
+    public double verificaNumeroPar(double a){
+        if(a % 2 == 0){
+            System.out.println("Numero Par");
+            return a;
+        }
+        System.out.println("Numero impar");
+        return a;
     }
 }
--- src/matheus/tbm/javalearning/javacore/Bintroductionmethods/test/CalculadoraTest.java
+++ src/matheus/tbm/javalearning/javacore/Bintroductionmethods/test/CalculadoraTest.java
@@ -5,7 +5,12 @@ import matheus.tbm.javalearning.javacore.Bintroductionmethods.dominio.Calculador
 public class CalculadoraTest {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        calculadora.somaDoisNumeros();
-        calculadora.subtraiDoisNumeros();
+        calculadora.somaDoisNumeros(15, 25);
+
+        calculadora.divideDoisNumeros(45, 67.5);
+        calculadora.multiplicaDoisNumeros(45, 67.5);
+
+        double result = calculadora.subtraiDoisNumeros(45, 67.5);
+        System.out.println(result);
     }
 }
