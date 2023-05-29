55804d335ed37eeebc8d6b4e2bb874f1c63c6cd1
Matheus <matheusfotografia987@gmail.com>
2023-05-29 12:44:34 -0300

    feat:add Class Product

---
+++ src/matheus/tbm/groceryStore/domains/Product.java
@@ -0,0 +1,98 @@
+package matheus.tbm.groceryStore.domains;
+
+import java.util.Objects;
+
+public class Product {
+
+    //attributes
+    private short productID;
+    private static short productCounter = 0;
+    private String name;
+    private String typeProduct;
+    private String description;
+    private double price;
+
+    {
+        productCounter++;
+        this.productID = productCounter;
+    }
+
+    //constructors
+    public Product(){}
+    public Product(String name){
+        this.name = name;
+    }
+    public Product(String name, String typeProduct){
+        this.name = name;
+        this.typeProduct = typeProduct;
+    }
+    public Product(String name, String typeProduct, String description){
+        this.name = name;
+        this.typeProduct = typeProduct;
+        this.description = description;
+    }
+    public Product(String name, String typeProduct, String description, double price){
+        this.name = name;
+        this.typeProduct = typeProduct;
+        this.description = description;
+        this.price = price;
+    }
+    public short getProductID() {
+        return productID;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTypeProduct() {
+        return typeProduct;
+    }
+    public void setTypeProduct(String typeProduct){
+        this.typeProduct = typeProduct;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (obj instanceof Product) {
+            Product p = (Product) obj;
+            return this.name.equals(p.getName()) || this.typeProduct.equals(p.getTypeProduct());
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productID, name, description, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productID=" + productID +
+                ", name='" + name + '\'' +
+                ", typeProduct='" + typeProduct + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
