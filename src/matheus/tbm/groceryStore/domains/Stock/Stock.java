c00af8a052e8c86a9ad18c841ac58433ded30e59
Matheus <matheusfotografia987@gmail.com>
2023-05-31 11:56:28 -0300

    feat:add two functions to removeProducts from stock and getProductById from stock

--- src/matheus/tbm/groceryStore/domains/Stock/Stock.java
+++ src/matheus/tbm/groceryStore/domains/Stock/Stock.java
@@ -2,15 +2,45 @@ package matheus.tbm.groceryStore.domains.Stock;
 
 import matheus.tbm.groceryStore.domains.Product.Product;
 
+
 public class Stock {
 
-    public final Product[] PRODUCTS = new Product[100];
+    public static final Product[] PRODUCTS = new Product[100];
     private static short stockCounter;
 
 
-    public static void addProduct(Stock stock, Product product){
+    //unique methods
+    public static Product getProductByID(short id){
+        if(PRODUCTS == null || id < 0 || id >= PRODUCTS.length){
+            System.out.println("Não é póssivel buscar esse produto!");
+        }
+
+        for(Product product : PRODUCTS){
+            if(product.getProductID() == id) return product;
+        }
+
+        return null;
+    }
+
+    public static void addProduct(Product product){
         stockCounter = (short) (Product.getProductCounter() - 1);
-        stock.PRODUCTS[stockCounter] = product;
+        PRODUCTS[stockCounter] = product;
+    }
+
+    public static void removeProduct(short id){
+        if(PRODUCTS == null || id < 0 || id >= PRODUCTS.length){
+            System.out.println("Não é possível remover nenhum produto do Estoque");
+            return;
+        }
+
+        for(short i = 0; i < PRODUCTS.length; i++){
+            if (PRODUCTS[i].getProductID() == id){
+                PRODUCTS[i] = null;
+                break;
+            }
+
+        }
+
     }
 
 }
