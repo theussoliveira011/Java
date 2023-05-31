ce532a70754b89147822d655d354ae864e141dd0
Matheus <matheusfotografia987@gmail.com>
2023-05-31 11:52:08 -0300

    feat:add method to give a discount in a Product

--- src/matheus/tbm/groceryStore/domains/Product/TypeProduct.java
+++ src/matheus/tbm/groceryStore/domains/Product/TypeProduct.java
@@ -1,12 +1,43 @@
 package matheus.tbm.groceryStore.domains.Product;
 
 public enum TypeProduct {
-    BOLACHA(1, "Bolacha"),
-    HIGIENE(2, "Higiene"),
-    ALIMENTO(3, "Alimento"),
-    FARINHA(4, "Farinha"),
-    FRUTA(5, "Fruta"),
-    LEGUME(6, "Legume");
+    BOLACHA(1, "Bolacha") {
+        @Override
+        public double discountToAProduct(double price) {
+            return price * 0.05;
+        }
+    },
+    HIGIENE(2, "Higiene"){
+        @Override
+        public double discountToAProduct(double price) {
+            return price * .05;
+        }
+
+    },
+    ALIMENTO(3, "Alimento"){
+        @Override
+        public double discountToAProduct(double price) {
+            return price * .1;
+        }
+    },
+    FARINHA(4, "Farinha"){
+        @Override
+        public double discountToAProduct(double price) {
+            return price * .04;
+        }
+    },
+    FRUTA(5, "Fruta"){
+        @Override
+        public double discountToAProduct(double price) {
+            return price * .09;
+        }
+    },
+    LEGUME(6, "Legume"){
+        @Override
+        public double discountToAProduct(double price) {
+            return price * 0.01;
+        }
+    };
 
     private int value;
     private String text;
@@ -18,5 +49,16 @@ public enum TypeProduct {
 
     public int getValue() {
         return value;
+    }
+
+    //uniques methods
+    public abstract double discountToAProduct(double value);
+
+    @Override
+    public String toString() {
+        return "TypeProduct{" +
+                "value=" + value +
+                ", text='" + text + '\'' +
+                '}';
     }
 }
